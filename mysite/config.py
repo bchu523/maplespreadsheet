12a0ee39ab1255d9fb938340af34f412500aa7b5
bchu523 <bchu526@gmail.com>
2017-09-10 00:50:31 -0700

    remove config.py

--- mysite/config.py
+++
@@ -1,6 +0,0 @@
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="maplespreadsheet",
-    password="mewmew79",
-    hostname="maplespreadsheet.mysql.pythonanywhere-services.com",
-    databasename="maplespreadsheet$votr",
-)
--- mysite/main.py
+++ mysite/main.py
@@ -194,7 +194,8 @@ def index():
 
 
 
-
+if __name__ == '__main__':
+    app.run(debug=False)
 
 
 
